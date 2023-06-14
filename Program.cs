7f11b5ae2deee5bf2a8fbaa2014539612ac46c2a
Arma Burton <arma@armaburton.com>
2023-06-14 15:13:36 -0700

    {non-generic types}

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ Person harry = new(){
     Name = "Harry", 
     DateOfBirth = new(year: 2001, month: 3, day: 25)
 };
+
+harry.WriteToConsole();
+
+//non-generic lookup collection
+System.Collections.Hashtable lookUpObject = new();
+lookUpObject.Add(key: 1, value: "Alpha");
+lookUpObject.Add(key: 2, value: "Beta");
+lookUpObject.Add(key: 3, value: "Gamma");
+lookUpObject.Add(key: harry, value: "Delta");
+
